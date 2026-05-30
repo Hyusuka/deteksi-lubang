43e658d16e73d55077219b9da7cf2483034cd435
Hyusuka <juanrahawarin12@gmail.com>
2026-05-30 17:49:14 +0700

    Fix ONNX LFS URL resolution

--- static/js/app.js
+++ static/js/app.js
@@ -332,8 +332,9 @@ async function loadONNXModel() {
         ort.env.wasm.numThreads = 1; 
         ort.env.wasm.wasmPaths = "https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/";
         
-        // Load model .onnx
-        ortSession = await ort.InferenceSession.create('/static/pothole_yolov8.onnx', {
+        // Load model .onnx via LFS Resolve
+        const modelUrl = 'https://huggingface.co/spaces/Hyusuka/Pothole_Scanner/resolve/main/static/pothole_yolov8.onnx';
+        ortSession = await ort.InferenceSession.create(modelUrl, {
             executionProviders: ['webgl', 'wasm'] // Prioritas GPU (WebGL), fallback CPU (WASM)
         });
         console.log('[ONNX] Model loaded successfully!');
